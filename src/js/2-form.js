d6f87166ac92f51d00c9c6052893b1c5c8226f89
Nadiia-Yuce <159655565+Nadiia-Yuce@users.noreply.github.com>
2024-06-21 22:48:52 +0200

    make form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,32 @@
-console.log('mother fucker!');
+const formData = {
+  email: '',
+  message: '',
+};
+const lokalStorageKey = 'feedback-form-state';
+
+function saveFormData() {
+  localStorage.setItem(lokalStorageKey, JSON.stringify(formData));
+}
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', handlerGetInfo);
+form.addEventListener('submit', handlerSubmitForm);
+
+function handlerGetInfo(evt) {
+  if (evt.target.name === 'email' || evt.target.name === 'message') {
+    formData[evt.target.name] = evt.target.value.trim();
+    saveFormData();
+  }
+}
+
+function handlerSubmitForm(evt) {
+  if (!formData.email || !formData.message) {
+    evt.preventDefault();
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem(lokalStorageKey);
+  formData = { email: '', message: '' };
+  form.reset();
+}
